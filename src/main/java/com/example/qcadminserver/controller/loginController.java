07648ca2b552fff79dbb9ed3cfcfd7ed24be7146
Xforeverm <1296754116@qq.com>
2023-06-11 16:53:57 +0800

    fix login

--- src/main/java/com/example/qcadminserver/controller/loginController.java
+++ src/main/java/com/example/qcadminserver/controller/loginController.java
@@ -7,6 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.UUID;
+
 /**
  * @author Xman
  * @version 1.0
@@ -20,11 +25,27 @@ public class loginController {
 
 
     @PostMapping("/login")
-    public ApiResult userLogin(@RequestBody user user){
-        System.out.println(user);
-        if (loginService.loginverify(user.getUsername(), user.getPassword())){
-            return ApiResult.success();
+    public ApiResult userLogin(@RequestBody user user) {
+        String token = UUID.randomUUID().toString().replace("-", "");
+        Map<String, Object> result = new HashMap<>();
+        result.put("token", token);
+        if (loginService.loginverify(user.getUsername(), user.getPassword())) {
+            return ApiResult.success(result);
         }
-        return ApiResult.fail();
+        return ApiResult.fail("username or password is wrong");
+    }
+
+    @GetMapping("/user/info")
+    public ApiResult getUserInfo() {
+        Map<String, Object> result = new HashMap<>();
+        result.put("name", "heihei");
+        result.put("avatar", "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");
+        return ApiResult.success(result);
+    }
+
+    @PostMapping("/user/logout")
+    public ApiResult userLogout() {
+        // if save token must remove token
+        return ApiResult.success();
     }
 }
