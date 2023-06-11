ebac5faf1eeb508d48897c7dc82040365dc5aa3b
Xforeverm <1296754116@qq.com>
2023-06-11 13:16:15 +0800

    project init

---
+++ src/main/java/com/example/qcadminserver/QcAdminServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.qcadminserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QcAdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QcAdminServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/qcadminserver/QcAdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qcadminserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QcAdminServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
